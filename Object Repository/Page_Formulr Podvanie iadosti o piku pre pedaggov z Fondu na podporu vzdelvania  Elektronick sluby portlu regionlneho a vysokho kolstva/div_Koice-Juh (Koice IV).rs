e82707e76c731f2566ea2d49ea545976051278e4
engelito94 <167324850+engelito94@users.noreply.github.com>
2024-04-18 10:42:03 +0200

    Add files via upload

---
+++ Object Repository/Page_Formulr Podvanie iadosti o piku pre pedaggov z Fondu na podporu vzdelvania  Elektronick sluby portlu regionlneho a vysokho kolstva/div_Koice-Juh (Koice IV).rs
@@ -0,0 +1,116 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Koice-Juh (Koice IV)</name>
+   <tag></tag>
+   <elementGuidId>7f4e9d34-43e3-46f8-995d-dea541934747</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div[value=&quot;119713&quot;]</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@value='119713']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>12036512-84a2-488b-9f70-ffaad0c83331</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>value</name>
+      <type>Main</type>
+      <value>119713</value>
+      <webElementGuid>4fdafc9b-af5a-49a9-9f9a-b20312b59cf3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Košice-Juh (Košice IV)</value>
+      <webElementGuid>73ccbda4-b947-4d91-b589-fcdc63e0df83</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;UI_V_ZP_in_AdresaKorespodencnaSlovenska_Obecautocomplete-list&quot;)/div[1]</value>
+      <webElementGuid>479419a5-bef1-4e0e-a891-ee9e9b79114d</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//div[@value='119713']</value>
+      <webElementGuid>78fb1586-052e-4579-9fb1-6df21a8a8b15</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='UI_V_ZP_in_AdresaKorespodencnaSlovenska_Obecautocomplete-list']/div</value>
+      <webElementGuid>65dd4d81-cb39-466a-8670-16e08ca1da95</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Vyhľadajte obec po zadaní prvých 3 znakov a vyberte obec zo zoznamu zobrazených obcí !'])[1]/following::div[2]</value>
+      <webElementGuid>f3c33731-e914-4de7-8077-b41c137b4df4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Zadajte obec alebo mesto, kam bude v prípade potreby zasielaná korešpondencia z Fondu.'])[1]/following::div[2]</value>
+      <webElementGuid>53331be5-7db0-41eb-8bfa-c2e836d11b4a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Ulica (nepovinný údaj)'])[2]/preceding::div[1]</value>
+      <webElementGuid>011b2807-7065-419c-9724-cfd87da2cf6c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='(Košice IV)']/parent::*</value>
+      <webElementGuid>9ef65990-d291-4376-ab67-e5b2311a3c0e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//fieldset/div/div/div</value>
+      <webElementGuid>d50c3763-f711-49c9-99e1-0adadc6b6d46</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = 'Košice-Juh (Košice IV)' or . = 'Košice-Juh (Košice IV)')]</value>
+      <webElementGuid>e6758f6b-d85e-4593-9b3c-4748b32cce07</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
