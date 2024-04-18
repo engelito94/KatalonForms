b6f70c701ff096aa78ca66d8cdf74c29a3a520f2
engelito94 <167324850+engelito94@users.noreply.github.com>
2024-04-18 10:36:18 +0200

    Add files via upload

---
+++ Object Repository/Page_Formulr iadosti o piku (bez zabezpeenia ruiteom)  Elektronick sluby portlu regionlneho a vysokho kolstva/div_Odobra.rs
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Odobra</name>
+   <tag></tag>
+   <elementGuidId>8035451b-cf6c-4532-9779-ff24c3ab4460</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>#vyber-suboru > div.govuk-form-group > div</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='vyber-suboru']/div/div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>b35d3c13-98a3-459b-93ba-1c0377d587a3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                
+                    
+                
+                
+                Odobrať
+            </value>
+      <webElementGuid>8d99047b-82dc-4025-b513-37292a927070</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;skutocnosti-prilohy-8&quot;)/fieldset[@class=&quot;govuk-fieldset&quot;]/div[@class=&quot;detail-prilohy-section&quot;]/div[@id=&quot;vyber-suboru&quot;]/div[@class=&quot;govuk-form-group&quot;]/div[1]</value>
+      <webElementGuid>a5f88088-2acf-4606-8d0e-e5ffeae881ff</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='vyber-suboru']/div/div</value>
+      <webElementGuid>d87c0e75-892d-4d75-bc2f-987dba90ec7b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Uložiť'])[2]/following::div[3]</value>
+      <webElementGuid>f0ef7b62-c7be-4ed9-976e-bc254fd4438b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Zrušiť'])[1]/following::div[3]</value>
+      <webElementGuid>6cca0744-8f1f-4cf6-af50-ea2add9e5bd4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Spoločná veľkosť priložených súborov nesmie presiahnuť 20 MB.'])[1]/preceding::div[2]</value>
+      <webElementGuid>78a36c27-e2af-46e2-81f3-fff0aa4327c2</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[8]/fieldset/div/div[2]/div/div</value>
+      <webElementGuid>6a766680-15b4-45e3-8712-08a8a2f26abc</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = '
+                
+                    
+                
+                
+                Odobrať
+            ' or . = '
+                
+                    
+                
+                
+                Odobrať
+            ')]</value>
+      <webElementGuid>7dcbcc32-95f4-48a9-bdfe-9353aa4f116d</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
